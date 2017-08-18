e0470a82361053afeacb9a9dba0ffc7889ce6990
Linghao Li <linghaol@usc.edu>
2017-08-17 23:38:39 -0700

    forth commit

--- myweb.py
+++ myweb.py
@@ -7,22 +7,29 @@ app = Flask(__name__)
 # index
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('blogpage.html')
 
 
 # blog page
+@app.route('/blogpage')
+def blog():
+	return render_template('blogpage.html')
 
-
-
-
-
-
+# stats
+@app.route('/stats')
+def stats():
+	return render_template('stats.html')
 
 
 # about me page
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 
 
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
