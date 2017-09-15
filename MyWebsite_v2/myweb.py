8858dfe199035a7ee87743ac9a1cbeb1c83a9d6c
linghaol <linghaol@usc.edu>
2017-09-15 08:31:25 -0700

    welcome page added

--- MyWebsite_v2/myweb.py
+++ MyWebsite_v2/myweb.py
@@ -7,25 +7,46 @@ from flask import json
 
 app = Flask(__name__)
 
-# index & blog
+# welcome
 @app.route('/')
+def getWelcome():
+	return render_template('welcome.html')
+
+# blogpage
+@app.route('/blogpage')
 def getBlogpage(itemList=None):
 	return render_template('blogpage.html', itemList=json.load(open('./content/parsed/bloglist.json')))
+
+@app.route('/blogpage-cn')
+def getBlogpage_cn(itemList=None):
+	return render_template('blogpage-cn.html', itemList=json.load(open('./content/parsed/bloglist.json')))
 
 # stats
 @app.route('/stats')
 def getStats():
 	return render_template('error.html')
 
+@app.route('/stats-cn')
+def getStats_cn():
+	return render_template('error-cn.html')
+
 # about me page
 @app.route('/about')
 def getAbout():
 	return render_template('error.html')
 
+@app.route('/about-cn')
+def getAbout_cn():
+	return render_template('error-cn.html')
+
 # render article
 @app.route('/blog/<article_title>')
 def getArticle(article_title):
 	return render_template('article.html', article=json.load(open('./content/parsed/'+article_title+'.json')))
+
+@app.route('/blog/<article_title>-cn')
+def getArticle_cn(article_title):
+	return render_template('article-cn.html', article=json.load(open('./content/parsed/'+article_title+'.json')))
 
 # error handle
 @app.errorhandler(404)
