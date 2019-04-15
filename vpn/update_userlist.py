d7cf05028b47751b16fc77518c29965c72f7562a
linghaol <linghaol@usc.edu>
2019-04-16 03:29:53 +0800

    v1.1

--- vpn/update_userlist.py
+++ vpn/update_userlist.py
@@ -3,10 +3,10 @@ import redis
 def update_ul():
 	with open('./data/userlist.txt', 'r') as f:
 		text = f.read().strip()
-		if text != '':
-			userlist = text.split('\n')
-	db = redis.Redis(host="100.0.0.2", port=6379)
-	db.sadd('userlist', *userlist)
+	if text != '':
+		userlist = text.split('\n')
+		db = redis.Redis(host="100.0.0.2", port=6379)
+		db.sadd('userlist', *userlist)
 
 if __name__ == '__main__':
 	update_ul()
